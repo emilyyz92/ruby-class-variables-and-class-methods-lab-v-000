bfaf218d8c968346443e6a1bb99fb7a3dcfd34c0
Emily Zhao <tong.zhao.92@gmail.com>
2017-10-29 23:14:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,8 +20,9 @@ class Song
   end
 
   def self.genres
+    binding.pry
     @@genre.each_with_index do |genre_name,index|
-      binding.pry
+
       if genre_name == @@genre[index+1]
         @@genre.delete(genre_name)
       end
