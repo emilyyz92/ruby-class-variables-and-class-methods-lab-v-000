854adbd8857e7b1261c3767af492d5efc0433493
Emily Zhao <tong.zhao.92@gmail.com>
2017-10-29 22:32:43 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+
+  def initialize
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  @@genre = []
+
+  def self.genre
+end
